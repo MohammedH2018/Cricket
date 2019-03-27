ad1f8d41a496de4059dd7ca18102566bfbff484b
Mohammed Hoche <mohammedhoche@Mohammeds-MacBook-Air.local>
2019-03-27 10:27:11 +0000

    first commit

---
+++ Cricket1/SignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInViewController.swift
+//  Cricket1
+//
+//  Created by Mohammed Hoche on 26/03/2019.
+//  Copyright © 2019 Mohammed Hoche. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cricket1/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Cricket1
+//
+//  Created by Mohammed Hoche on 26/03/2019.
+//  Copyright © 2019 Mohammed Hoche. All rights reserved.
+//
+
+import Foundation
